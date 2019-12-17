37c93a0c11546659135b77ad839adbd0f24cc66f
Jorge Gemas Herencia <jorgegh2@TR12.CAT>
2019-12-17 13:29:29 +0100

    Create project and link firebase

---
+++ dungeons_and_dragons_order_turns/android/app/src/main/kotlin/com/example/dungeons_and_dragons_order_turns/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dungeons_and_dragons_order_turns
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
