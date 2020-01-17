3593c74e0c9f3bd5d805cd2015f923ca9839a595
Jorge <jgherencia@gmail.com>
2020-01-17 21:25:02 +0100

    order turn with "Pass Turn" and turn counter done

--- dungeons_and_dragons_order_turns/lib/main.dart
+++ dungeons_and_dragons_order_turns/lib/main.dart
@@ -13,34 +13,76 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int current_turn = 0;
   @override
   Widget build(BuildContext context) {
     final db = Firestore.instance;
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red,
         title: Text("MainScreen"),
       ),
-      body: StreamBuilder(
-        stream: db.collection('test').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          }
-          if (!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          List<DocumentSnapshot> docs = snapshot.data.documents;
-          return ListView.builder(
-              itemCount: docs.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(docs[index].data["class"]),
-                );
-              });
-        },
+      body: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              FlatButton(
+                color: Colors.pink[100],
+                child: Text("Pass Turn"),
+                onPressed: () {
+                  db.collection('Characters').getDocuments().then((snapshot) {
+                    for (DocumentSnapshot ds in snapshot.documents) {
+                      Map<String, dynamic> newData = new Map<String, dynamic>();
+                      newData['Turn'] = ds.data['Turn'] - 1;
+                      if(newData['Turn'] == 0)
+                      newData['Turn'] = snapshot.documents.length;
+                      ds.reference.updateData(newData);
+                      if(ds.data['OriginalFirst'] && newData['Turn'] == 1)
+                      {
+                        setState(() {
+                          ++current_turn;
+                        });
+                      }
+                    }
+                  });
+                },
+              ),
+              Text("Turn: $current_turn"),
+            ],
+          ),
+          Expanded(
+            child: StreamBuilder(
+              stream: db.collection('Characters').orderBy("Turn").snapshots(),
+              builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                }
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                List<DocumentSnapshot> docs = snapshot.data.documents;
+                return ListView.builder(
+                    itemCount: docs.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(docs[index].data["CharacterName"]),
+                        subtitle: Text(
+                            docs[index].data["InitiativeValue"].toString()),
+                      );
+                    });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
